8f7272339f61219b8ddb0583437e4e90bc8bc198
1romang1 <krglrv@gmail.com>
2024-11-07 18:58:54 +0300

    added src/js/init.js

---
+++ src/js/init.js
@@ -0,0 +1,38 @@
+import * as yup from "yup";
+
+const initialState = {
+  addedUrls: ["https://yandex.ru"],
+//   url: "https://google.com",
+  addingUrlProcess: {
+    processState: "filing", // варианты: 'filling', 'error', 'added', 'addition'
+    processError: null,
+  },
+  form: {
+    valid: true,
+    errors: {},
+    fields: {
+      url: "https://google.com",
+    },
+  },
+};
+
+const schema = yup.object({
+  url: yup
+    .string()
+    .url("Ссылка должна быть валидным URL")
+    .required("Это поле обязательно для заполнения")
+    .test(
+      "unique-url",
+      "Ссылка уже существует",
+      (value) => !initialState.addedUrls.includes(value),
+    ),
+});
+
+const validate = (url) => {
+  schema
+    .validate(url)
+    .then(() => console.log("фид валидный"))
+    .catch((error) => console.log(error.errors));
+};
+
+validate(initialState.form.fields);
