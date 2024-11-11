13438dd76a834a61014664aa70257ce1ef8f7542
1romang1 <krglrv@gmail.com>
2024-11-11 17:32:35 +0300

    added some functions in src/js/view.js

--- src/js/view.js
+++ src/js/view.js
@@ -1,3 +1,5 @@
-export const render = (inputElement, initialState) => {
+export const handleProcessState = (elements, processState) => {};
 
-}
+export const renderError = (fieldElement, error) => {};
+
+export const render = (elements, initialState) => {};
