0f8eb953399adda3cc71abba49be6ba396111217
1romang1 <krglrv@gmail.com>
2024-11-13 18:22:21 +0300

    added watchedState, render

--- src/js/init.js
+++ src/js/init.js
@@ -1,7 +1,7 @@
 import onChange from 'on-change';
 import * as yup from 'yup';
 // import isEmpty from 'lodash/isEmpty.js';
-import { render } from './view.js';
+import render from './view.js';
 
 const initialState = {
   addedUrls: [],
@@ -17,14 +17,6 @@ const initialState = {
     },
   },
 };
-
-const elements = {
-  form: document.querySelector('form'),
-  urlInput: document.getElementById('url-input'),
-  submitButton: document.querySelector('button'),
-  feedbackElement: document.querySelector('.feedback'),
-};
-
 const schema = yup.object({
   url: yup
     .string()
@@ -37,29 +29,36 @@ const schema = yup.object({
     ),
 });
 
-// const watchedState = onChange(initialState, render(elements, initialState));
-
 export default () => {
+  const elements = {
+    form: document.querySelector('form'),
+    urlInput: document.getElementById('url-input'),
+    submitButton: document.querySelector('button'),
+    feedbackElement: document.querySelector('.feedback'),
+  };
+
+  const watchedState = onChange(initialState, render(elements, initialState));
+
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const urlInputValue = elements.urlInput.value;
-    initialState.form.fields.url = urlInputValue;
+    watchedState.form.fields.url = urlInputValue;
 
     schema
       .validate(initialState.form.fields)
       .then(() => {
-        initialState.addingUrlProcess.processState = 'added'; // меняем статус процесса для render()
-        initialState.addedUrls.push(urlInputValue); // добавляем валидный url в подписки
-        initialState.form.fields.url = ''; // очищаем поле ввода
-        initialState.form.errors = {}; // сбрасываем ошибки
+        watchedState.addingUrlProcess.processState = 'added'; // меняем статус процесса для render()
+        watchedState.addedUrls.push(urlInputValue); // добавляем валидный url в подписки
+        // watchedState.form.errors = {}; // сбрасываем ошибки
       })
       .catch((err) => {
-        initialState.addingUrlProcess.processState = 'error';
-        initialState.form.errors = err.message;
+        watchedState.addingUrlProcess.processState = 'error';
+        watchedState.form.errors = err.message;
       })
       .finally(() => {
         console.log(initialState);
+        console.log(elements);
       });
   });
 };
--- src/js/view.js
+++ src/js/view.js
@@ -1,5 +1,14 @@
-export const handleProcessState = (elements, processState) => {};
-
-export const renderError = (fieldElement, error) => {};
-
-export const render = (elements, initialState) => {};
+export default (elements, initialState) => {
+  switch (initialState.addingUrlProcess.processState) {
+    case 'added':
+      elements.feedbackElement.textContent = 'RSS успешно загружен';
+      elements.urlInput.value = '';
+      break;
+    case 'error':
+      elements.urlInput.classList.add('is-invalid');
+      break;
+    default:
+      //   throw new Error('Unknown value');
+      break;
+  }
+};
