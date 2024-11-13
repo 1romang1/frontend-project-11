bcffd85b1f60d0506652cd46f23a7a2beb17c221
1romang1 <krglrv@gmail.com>
2024-11-13 18:41:24 +0300

    fix feedback element 2

--- src/js/view.js
+++ src/js/view.js
@@ -1,11 +1,17 @@
-export default (elements, initialState) => {
+export default (elements, initialState) => () => {
   switch (initialState.addingUrlProcess.processState) {
     case 'added':
       elements.feedbackElement.textContent = 'RSS успешно загружен';
+      elements.feedbackElement.classList.remove('text-danger');
+      elements.urlInput.classList.remove('is-invalid');
+      elements.feedbackElement.classList.add('text-success');
       elements.urlInput.value = '';
       break;
     case 'error':
       elements.urlInput.classList.add('is-invalid');
+      elements.feedbackElement.textContent = initialState.form.errors;
+      elements.feedbackElement.classList.remove('text-success');
+      elements.feedbackElement.classList.add('text-danger');
       break;
     default:
       //   throw new Error('Unknown value');
