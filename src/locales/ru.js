9413917b9d608e0f42f5cf109ecab85ad64fea1e
Roma <krglrv@gmail.com>
2024-11-27 15:11:57 +0300

    filled src/locales/index.js and src/locales/ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    submitBtn: 'Добавить',
+    validFeedback: 'RSS успешно загружен',
+    errors: {
+      required: 'Это поле обязательно для заполнения',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      linkExists: 'Ссылка уже существует',
+    },
+  },
+};
