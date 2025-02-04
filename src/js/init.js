a3a480b9dbe7a6fe2609be9914581ff884634012
1romang1 <krglrv@gmail.com>
2025-02-04 17:46:37 +0300

    search feed items (not finished)

--- src/js/init.js
+++ src/js/init.js
@@ -1,38 +1,39 @@
-import onChange from "on-change";
-import * as yup from "yup";
-import i18next from "i18next";
-import axios from "axios";
+import onChange from 'on-change';
+import * as yup from 'yup';
+import i18next from 'i18next';
+import axios from 'axios';
 // import isEmpty from 'lodash/isEmpty.js';
-import render from "./view.js";
-import resources from "../locales/index.js";
+import render from './view.js';
+import resources from '../locales/index.js';
 
 export default () => {
   const initialState = {
     addedUrls: [],
     addingUrlProcess: {
-      processState: "filling", // варианты: 'filling', 'error', 'added'
+      processState: 'filling', // варианты: 'filling', 'error', 'added'
       // processError: null, // для сетевых ошибок
     },
     form: {
       // valid: true,
       errors: {},
       fields: {
-        url: "",
+        url: '',
       },
     },
+
   };
 
   const elements = {
-    form: document.querySelector("form"),
-    urlInput: document.getElementById("url-input"),
-    submitButton: document.querySelector("button"),
-    feedbackElement: document.querySelector(".feedback"),
+    form: document.querySelector('form'),
+    urlInput: document.getElementById('url-input'),
+    submitButton: document.querySelector('button'),
+    feedbackElement: document.querySelector('.feedback'),
   };
 
   const i18nextInstance = i18next.createInstance();
   i18nextInstance
     .init({
-      lng: "ru",
+      lng: 'ru',
       debug: true,
       resources,
     })
@@ -40,15 +41,15 @@ export default () => {
       yup.setLocale({
         string: {
           url: () => ({
-            key: "errors.invalidUrl",
+            key: 'errors.invalidUrl',
           }),
         },
         mixed: {
           required: () => ({
-            key: "errors.required",
+            key: 'errors.required',
           }),
           notOneOf: () => ({
-            key: "errors.linkExists",
+            key: 'errors.linkExists',
           }),
         },
       });
@@ -68,7 +69,7 @@ export default () => {
           newUrl
         )}`;
 
-      elements.form.addEventListener("submit", (e) => {
+      elements.form.addEventListener('submit', (e) => {
         e.preventDefault();
 
         const urlInputValue = elements.urlInput.value;
@@ -79,7 +80,7 @@ export default () => {
         schema
           .validate(watchedState.form.fields)
           .then(() => {
-            watchedState.addingUrlProcess.processState = "added"; // меняем статус процесса для render()
+            watchedState.addingUrlProcess.processState = 'added'; // меняем статус процесса для render()
             watchedState.addedUrls.push(urlInputValue); // добавляем валидный url в подписки
             // watchedState.form.errors = {}; // сбрасываем ошибки
             console.log(initialState);
@@ -87,31 +88,35 @@ export default () => {
               .get(createProxyUrl(initialState.form.fields.url))
               .then((response) => {
                 if (response.status === 200) return response.data;
-                throw new Error("Network response was not ok.");
+                throw new Error('Network response was not ok.');
               })
               .then((data) => {
                 // console.log(response.status);
                 const newData = data.contents;
                 const parser = new DOMParser();
                 // const xmlDoc = parser.parseFromString(newData, 'application/xml');
-                return parser.parseFromString(newData, "application/xml");
+                return parser.parseFromString(newData, 'application/xml');
               })
               .then((xmlDoc) => {
-                const items = xmlDoc.querySelectorAll("item");
+                const items = xmlDoc.querySelectorAll('item');
                 items.forEach((item) => {
-                  const title = item.querySelector("title").textContent;
-                  const link = item.querySelector("link").textContent;
-                  const description =
-                    item.querySelector("description").textContent;
-                  console.log(`Title: ${title}`);
-                  console.log(`Link: ${link}`);
-                  console.log(`Description: ${description}`);
-                  console.log("---");
+                  console.log(item.children)
+                  
+                  // const title = item.querySelector('title').textContent;
+                  // const description =
+                  //   item.querySelector('description').textContent;
+                  // console.log(`Title: ${title}`);
+                  // console.log(`Description: ${description}`);
+                  // console.log('---');
                 });
+                const channel = xmlDoc.querySelectorAll('channel');
+               
+                  console.log(channel.title)
+      
               });
           })
           .catch((err) => {
-            watchedState.addingUrlProcess.processState = "error";
+            watchedState.addingUrlProcess.processState = 'error';
             watchedState.form.errors = err.message;
             console.log(JSON.stringify(err, null, 2));
             console.log(watchedState.addedUrls);
