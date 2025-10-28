8b1cf08e02cf97501fd2b87a9cfcaa38c061d1d5
Roma <krglrv@gmail.com>
2025-10-28 10:01:47 +0300

    add src/js/utils/fetchRSS.js

---
+++ src/js/utils/fetchRSS.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default (url) =>
+  axios
+    .get(
+      `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
+        url
+      )}`,
+    )
+    .then((res) => res.data.contents)
+    .catch((err) => {
+      throw new Error(`Ошибка при скачивании RSS: ${err.message}`);
+    });
