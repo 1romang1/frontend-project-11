3009495c2eda2cfe7d96cfd69601c63fb15c8578
Roma <krglrv@gmail.com>
2025-10-29 12:16:45 +0300

    add src/js/utils/createSchema.js

---
+++ src/js/utils/createSchema.js
@@ -0,0 +1,33 @@
+import * as yup from "yup";
+import fetchRSS from "./fetchRSS";
+
+const parceRSS = (data) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data.contents, "applicatoin/xml");
+
+  const isRss = doc.querySelector("rss");
+  const isAtom = doc.querySelector("feed");
+
+  if (!isRss && !isAtom) {
+    return false;
+  }
+  return true;
+};
+
+const isRSS = (url) => fetchRSS(url).then((data) => parceRSS(data)); // возвращает булево значение
+
+export default (validatedUrl) =>
+  yup.object({
+    url: yup
+      .string()
+      .url()
+      .required()
+      .notOneOf(validatedUrl)
+      .test("is-rss", "Ресурс не содержит валидный RSS", (value) => {
+        if (!value) return false;
+        // Используем оригинальную isRSS
+        return isRSS(value)
+          .then(() => true) // если парсинг удался — всё ок
+          .catch(() => false); // если ошибка — не RSS
+      }),
+  });
