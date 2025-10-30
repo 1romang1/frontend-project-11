2c021cf4f1d4be48c1652055d09c66943851cfa9
Roma <krglrv@gmail.com>
2025-10-30 19:51:48 +0300

    fix parserRSS.js

--- src/js/utils/parserRSS.js
+++ src/js/utils/parserRSS.js
@@ -1,6 +1,6 @@
 export default (data) => {
   const parser = new DOMParser();
-  const result = parser.parseFromString(data.contents, 'applicatoin/xml');
+  const result = parser.parseFromString(data.contents, 'application/xml');
   const parseError = result.getElementsByTagName('parserror');
 
   if (parseError.length > 0) {
