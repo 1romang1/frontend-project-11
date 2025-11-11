1c954e082c712e57043a62f40dddc038d3e2a02a
1romang1 <krglrv@gmail.com>
2025-11-11 17:31:51 +0300

    fix src/js/utils/parserRSS.js

--- src/js/utils/parserRSS.js
+++ src/js/utils/parserRSS.js
@@ -1,8 +1,8 @@
 export default (data) => {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(data.contents, 'application/xml');
+  const doc = parser.parseFromString(data, 'application/xml');
 
-  const parseError = doc.getElementsByTagName('parserror');
+  const parseError = doc.getElementsByTagName('parsererror');
   if (parseError) {
     throw new Error(parseError[0].textContent);
   }
