cd5919e2184eb19f82cb103f6ce4e18855935f54
Roma <krglrv@gmail.com>
2025-12-15 15:59:29 +0300

    Revert "error search"
    This reverts commit ab43edc30b2b71eb3dfed7f7f8e1ef6527c18c07.

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -6,8 +6,8 @@ export default {
       required: 'Это поле обязательно для заполнения',
       invalidUrl: 'Ссылка должна быть валидным URL',
       linkExists: 'RSS уже существует',
-      network: 'Ошибка сети',
       notRSS: 'Ресурс не содержит валидный RSS',
+      network: 'Ошибка сети',
     },
   },
 };
