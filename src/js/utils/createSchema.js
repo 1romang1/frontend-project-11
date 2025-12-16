7fa5cec91f3662754f57209471d38ab7499f05ed
Roma <krglrv@gmail.com>
2025-12-16 18:19:26 +0300

    fix isRSS

--- src/js/utils/createSchema.js
+++ src/js/utils/createSchema.js
@@ -17,18 +17,7 @@ const parseRSS = (data) => {
 };
 
 // Проверка, является ли URL RSS
-const isRSS = (url) => fetchRSS(url).then((data) => {
-  try {
-    // Включим временный лог для диагностики
-    // console.log("Проверка URL:", url);
-    // console.log("Тип данных:", typeof data);
-    // console.log("Начало данных:", data.slice(0, 200));
-    return parseRSS(data);
-  } catch (err) {
-    console.error('Ошибка парсинга:', err.message);
-    throw new Error('errors.network');
-  }
-});
+const isRSS = (url) => fetchRSS(url).then((data) => parseRSS(data));
 
 export default (validatedUrl) => yup.object({
   url: yup
@@ -39,8 +28,8 @@ export default (validatedUrl) => yup.object({
     .test('is-rss', { key: 'errors.notRSS' }, (value) => {
       if (!value) return false;
       // Используем оригинальную isRSS
-      return isRSS(value)
-        .then(() => true) // если парсинг удался — всё ок
-        .catch(() => false); // если ошибка — не RSS
+      return isRSS(value);
+      // .then(() => true) // если парсинг удался — всё ок
+      // .catch(() => false); // если ошибка — не RSS
     }),
 });
